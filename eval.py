8541286fd5a1b0f7bf43bbc77eea30f9c5b6d019
Boris Polonsky <BorisPolonsky@users.noreply.github.com>
2018-11-06 16:23:50 +0800

    Incompatible Change
    Character "tab" is now used as delimiter instead when saving and evaluating tagged sentences.

--- eval.py
+++ eval.py
@@ -1,12 +1,13 @@
 import os
 
 
-def conlleval(label_predict, label_path, metric_path):
+def conlleval(label_predict, label_path, metric_path, delimiter="\t"):
     """
 
     :param label_predict:
     :param label_path:
     :param metric_path:
+    :param delimiter
     :return:
     """
     eval_perl = "./conlleval_rev.pl"
@@ -15,10 +16,14 @@ def conlleval(label_predict, label_path, metric_path):
         for sent_result in label_predict:
             for char, tag, tag_ in sent_result:
                 char = char.encode("utf-8")
-                line.append("{} {} {}\n".format(char, tag, tag_))
+                line.append("{}{delimiter}{}{delimiter}{}\n".format(char, tag, tag_, delimiter=delimiter))
             line.append("\n")
         fw.writelines(line)
-    os.system("perl {} -o O < {} > {}".format(eval_perl, label_path, metric_path))
+    delimiter = delimiter.replace("\t", r'"\t"').replace(" ", '" "')
+    if delimiter != " ":
+        os.system("perl {} -o O -d {} < {} > {}".format(eval_perl, delimiter, label_path, metric_path))
+    else:
+        os.system("perl {} -o O < {} > {}".format(eval_perl, label_path, metric_path))
     with open(metric_path) as fr:
         metrics = [line.strip() for line in fr]
     return metrics
